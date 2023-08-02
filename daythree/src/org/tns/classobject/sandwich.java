57b2a9649ba348f5f150fcbf044e0f14e9e76aec
Itsmhaske <sakshimhaske609@gmail.com>
2023-08-02 15:24:06 +0530

    first commit

---
+++ daythree/src/org/tns/classobject/sandwich.java
@@ -0,0 +1,12 @@
+package org.tns.classobject;
+//class declaration
+public class sandwich {
+	//default data members
+	  String breadType;
+	  int noofslice;
+      void display()
+	{
+		System.out.println("BreadType: "+breadType+" "+"No.of slice:"+noofslice);
+	}
+
+}
